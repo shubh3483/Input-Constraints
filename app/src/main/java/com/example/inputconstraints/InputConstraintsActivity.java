2bfb81fa8166025860aa49e235fca6e4d73ce1f8
Shubh Agrawal <shubh.coding@gmail.com>
2021-05-06 23:51:59 +0530

    Removed Dark mode option

--- app/src/main/java/com/example/inputconstraints/InputConstraintsActivity.java
+++ app/src/main/java/com/example/inputconstraints/InputConstraintsActivity.java
@@ -1,12 +1,12 @@
 package com.example.inputconstraints;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.inputconstraints.databinding.ActivityInputConstraintsBinding;
 
